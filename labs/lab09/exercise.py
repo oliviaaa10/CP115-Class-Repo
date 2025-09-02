b2a3b36f1ab747aaf74f1e4fc628535360eb61c6
oliviaaa10 <oliviapolus10@gmail.com>
2025-09-02 11:54:54 +0800

    update

---
+++ labs/lab09/exercise.py
@@ -0,0 +1,13 @@
+# else positioning - always last
+student_grade = 45
+
+if student_grade >= 90:
+    print("Excellent performance")
+elif student_grade >= 80:
+    print("Good performance")
+elif student_grade >= 70:
+    print("Satisfactory performance") 
+elif student_grade >= 60:
+    print("Minimum performance")
+else:                        # Always positioned last
+    print("Below minimum performance")
